99c239f0c93f40480cdd929d6684017b62d00216
朱永春 <zuygcn@163.com>
2023-01-17 21:58:53 +0800

    refactor: 删除测试代码

--- test/test.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('test.html', 'r', encoding='utf8') as f:
-    bs = BeautifulSoup(f.read(), 'lxml')
-    text = bs.find(id='activity-name').text
-    print(text.strip())
